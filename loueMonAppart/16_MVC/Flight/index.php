45e7be3f59994655eb5a64bfbffae67abeced11a
yohann <ym66300@gmail.com>
2017-08-03 09:55:49 +0200

    debutenfer

---
+++ loueMonAppart/16_MVC/Flight/index.php
@@ -0,0 +1,50 @@
+<?php
+
+require 'flight/Flight.php';
+
+
+Flight::route('/', function(){
+    echo 'hello world!';
+
+});
+
+Flight::route('/login', function(){
+    Flight::render('login');
+
+});
+
+
+Flight::start();
+
+
+
+
+
+
+
+
+
+
+// if(isset($_GET['page'])){
+//     $page = $_GET['page'];
+// }
+// else{
+//     $page = 'login';
+// }
+// switch($page){
+//     case 'login':
+//     // 1) Dans un premier temps, avoir nos données
+//     // 2) Dans un second temps, remplir notre template avec ces données
+//     include("views/login.php");
+//     break;
+
+//     case 'wall';
+//     include("views/wall.php");
+//     break;
+
+//     case 'test';
+//     include("views/test.php");
+//     break;
+// }
+
+?>
